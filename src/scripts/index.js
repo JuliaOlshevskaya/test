72b65c9b39c5b2a0151c6d15755405f21857f22b
JuliaOlshevska <juliafaer2@gmail.com>
2019-11-09 16:08:28 +0200

    JS-learning

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,31 @@
 import "../styles/index.scss";
 import "../styles/test.scss";
 
-//alert('Test');
+const a = 12;
+const b = 4;
+const addition = a + b;
+console.log(addition);
+const subtraction = a - b;
+console.log(subtraction);
+const division = a / b;
+console.log(division);
+const multiplication = a * b;
+console.log(multiplication);
 
-console.log("webpack starterkit");
+const user = {
+  name: "Julia",
+  surname: "Faer",
+  birthday: "20.01.2000",
+  age: 19,
+  height: 1.68,
+  favouriteFood: ["salad", "chicken", "cake"],
+  pet: true,
+  getNameSurnameBirthday: function() {
+    console.log(
+      "getNameSurnameBirthday",
+      "Julia" + " " + "Faer" + " " + "20.01.2000"
+    );
+  }
+};
+console.log(user);
+user.getNameSurnameBirthday();
