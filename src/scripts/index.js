105bed8e9bcc0624ee2e88880fe062bf80b9ccc0
JuliaOlshevska <juliafaer2@gmail.com>
2019-11-23 21:22:50 +0200

    Bootstrap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,5 @@
 import "../styles/index.scss";
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 import "../styles/test.scss";
 import "./events";
-
-const a = 12;
-const b = 4;
-const addition = a + b;
-console.log(addition);
-const subtraction = a - b;
-console.log(subtraction);
-const division = a / b;
-console.log(division);
-const multiplication = a * b;
-console.log(multiplication);
-
-const user = {
-  name: "Julia",
-  surname: "Faer",
-  birthday: "20.01.2000",
-  age: 19,
-  height: 1.68,
-  favouriteFood: ["salad", "chicken", "cake"],
-  pet: true,
-  getNameSurnameBirthday: function() {
-    console.log(
-      "getNameSurnameBirthday",
-      this.name + " " + this.surname + " " + this.birthday
-    );
-  }
-};
-console.log(user);
-user.getNameSurnameBirthday();
