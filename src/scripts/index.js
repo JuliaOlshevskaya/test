d02cfdd1ac900531b76a8c2e9da4c3d3e613607b
JuliaOlshevska <juliafaer2@gmail.com>
2019-12-13 21:44:15 +0200

    Problems

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,56 +3,31 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "../styles/test.scss";
 import "./events";
-const age = 17;
-age >= 18 && age <= 21
-  ? console.log(
-      "I am younger than 18, so you can see this masage in your console"
-    )
-  : console.log("I am 18, or even older!");
 
-// 1) Проверьте переменную age и если возраст больше или равен 18 и меньше или равен 21, выведите в консоль соответсвующее сообщение
-
-const users = [
-  {
-    name: "Roman",
-    age: 28,
-    visitedPark: true,
-    boughtItems: false
-  },
-  {
-    name: "Maya",
-    age: 14,
-    visitedPark: false,
-    boughtItems: true
-  },
-  {
-    name: "Alex",
-    age: 45,
-    visitedPark: false,
-    boughtItems: false
-  },
-  {
-    name: "Tonia",
-    age: 32,
-    visitedPark: true,
-    boughtItems: true
-  }
-];
-
-users.forEach(user => {
-  if (user.visitedPark || user.boughtItems) {
-    console.log("I was in the park, or made a shopping");
-  }
-});
-
-// 2) Для каждого элемента массива users выведите сообщение в консоль, только если пользователь посетил парк или совершил покупку
-// Используйте foreach или for, а потом if внутри петли
-
-let iterationCounter = "Each iteration adds a *: ";
-for (let i = 0; i <= 10; i++) {
-  iterationCounter += " *";
+function displayData() {
+  const ul = document.querySelector("#clientsData");
+  clients.forEach(client => {
+    const newLi = document.createElement("li");
+    newLi.className = "media";
+    const avatar = document.createElement("img");
+    avatar.className = "mr-3 align-self-center";
+    const div = document.createElement("div");
+    div.className = "media-body";
+    avatar.setAttribute("src", client.avatar);
+    const text = document.createTextNode(
+      `${client.firstName} ${client.lastName} - ${client.email}, ${client.gender} (${client.date} - ${client.amount})`
+    );
+    div.appendChild(text);
+    newLi.appendChild(avatar);
+    newLi.appendChild(div);
+    ul.appendChild(newLi);
+  });
 }
-console.log(iterationCounter);
-// 3) Используя assigment operator(+=) поменяйте значение переменоой iterationCounter, добавляя по одной звездочке при каждой итерации
 
-// 4) Выведите в консоль значение iterationCounter. Ожидаемый результат "Each iteration adds a *: * * * * * * * * * *"
+function sortList(order) {
+  if (order == "ascending") {
+    console.log("ascending");
+  } else {
+    console.log("descending");
+  }
+}
