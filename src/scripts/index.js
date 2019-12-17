d6f9954a237244fa1ddc1c0c0c39dbcdb35d7135
JuliaOlshevska <juliafaer2@gmail.com>
2019-12-17 13:56:14 +0200

    New

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,10 +28,30 @@ function getLiElement(client) {
   ul.appendChild(newLi);
 }
 
-function sortList(order) {
-  order == "ascending" ? console.log("ascending") : console.log("descending");
-  const sortedClients = clients.sort((lastClient, nextClient) => {});
+function sortList(clients) {
+  const sortedClients = clients.sort((lastClient, nextClient) => {
+    if (order == "ascending") {
+      lastClient.lastName > nextClient.lastName ? 1 : -1;
+    } else {
+      lastClient.lastName < nextClient.lastName ? 1 : -1;
+    }
+  });
+  console.table(sortedClients);
+  refreshData();
+}
+function refreshData() {
+  clearList();
+}
+
+function clearList() {
+  const ul = document.querySelector("#clientsData");
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
 }
 
 window.displayData = displayData;
+window.getLiElement = getLiElement;
 window.sortList = sortList;
+window.refreshData = refreshData;
+window.clearList = clearList;
