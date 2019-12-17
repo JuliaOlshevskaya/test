d09a95ae06a3d63e41f75db54a1a7df755e4aa56
JuliaOlshevska <juliafaer2@gmail.com>
2019-12-17 12:57:39 +0200

    Problems with Names

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,31 +3,35 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "../styles/test.scss";
 import "./events";
+import "./data";
 
 function displayData() {
   const ul = document.querySelector("#clientsData");
   clients.forEach(client => {
-    const newLi = document.createElement("li");
-    newLi.className = "media";
-    const avatar = document.createElement("img");
-    avatar.className = "mr-3 align-self-center";
-    const div = document.createElement("div");
-    div.className = "media-body";
-    avatar.setAttribute("src", client.avatar);
-    const text = document.createTextNode(
-      `${client.firstName} ${client.lastName} - ${client.email}, ${client.gender} (${client.date} - ${client.amount})`
-    );
-    div.appendChild(text);
-    newLi.appendChild(avatar);
-    newLi.appendChild(div);
-    ul.appendChild(newLi);
+    ul.appendChild(getLiElement(client));
   });
+}
+function getLiElement(client) {
+  const newLi = document.createElement("li");
+  const avatar = document.createElement("img");
+  const div = document.createElement("div");
+  newLi.className = "media";
+  avatar.className = "mr-3 align-self-center";
+  div.className = "media-body";
+  avatar.setAttribute("src", client.avatar);
+  const text = document.createTextNode(
+    `${client.lastName} ${client.firstName} - ${client.email}, ${client.gender} (${client.date} - ${client.amount})`
+  );
+  div.appendChild(text);
+  newLi.appendChild(avatar);
+  newLi.appendChild(div);
+  ul.appendChild(newLi);
 }
 
 function sortList(order) {
-  if (order == "ascending") {
-    console.log("ascending");
-  } else {
-    console.log("descending");
-  }
+  order == "ascending" ? console.log("ascending") : console.log("descending");
+  const sortedClients = clients.sort((lastClient, nextClient) => {});
 }
+
+window.displayData = displayData;
+window.sortList = sortList;
